96c48e383460f1d29e76f60fb008ae3e29bbebff
Guillaume Galy <ggaly@gatling.io>
2024-03-12 14:27:28 +0100

    chore: Remove Maven Archetype

--- archetype/src/main/resources/archetype-resources/src/main/ts/main.ts
+++
@@ -1,9 +0,0 @@
-
-
-import {Stream,Arrays,URI,Collectors} from "${typescript-filename}-types";
-
-
-
-let result = Stream.of( "HELLO", "JAVA2TS!").collect( Collectors.joining(" "));
-
-print( result );
